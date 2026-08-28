8e930d7e592b72d83bde1bb587ceb68417647301
pavluk001 <285793469+pavluk010@users.noreply.github.com>
2026-08-28 10:53:11 +0300

    fix

--- mobile.js
+++ mobile.js
@@ -2,20 +2,20 @@ const menuOpenBtn = document.querySelector('.menu-open-btn');
 const menuCloseBtn = document.querySelector('.menu-close-btn');
 const menuModal = document.querySelector('.menu-modal');
 
+console.log(menuOpenBtn);
+console.log(menuCloseBtn);
+console.log(menuModal);
 
 menuOpenBtn.addEventListener('click', () => {
   menuModal.classList.add('is-open');
 });
 
-
 menuCloseBtn.addEventListener('click', () => {
   menuModal.classList.remove('is-open');
 });
 
-
-menuModal.addEventListener('click', (event) => {
+menuModal.addEventListener('click', event => {
   if (event.target === menuModal) {
     menuModal.classList.remove('is-open');
   }
 });
-
